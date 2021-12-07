6ac315be2340e3ef68641f7a488cea2e60849077
Cleber <cleberrobson16@gmail.com>
2021-12-06 22:43:57 -0300

    alguns ajustes na view e importação do php

---
+++ formulario.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$tel = $_POST['tel'];
+$endereco = $_POST['endereco'];
+$projeto = $_POST['projeto'];
+$social = $_POST['social'];
+$description = $_POST['description'];
+
+print_r($nome);
